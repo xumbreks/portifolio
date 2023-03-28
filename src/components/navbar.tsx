cb22cd80b7abdceeaface83e720cac9eff77121a
BRIAN <owner@MBP-de-Brian.domain.name>
2023-03-28 18:54:42 -0300

    navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -8,45 +8,52 @@ export function Navbar() {
 
   return (
     <>
-      <nav className="flex flex-col h-auto sm:h-16 w-full justify-center items-center fixed">
-        <div className="w-auto sm:flex justify-center items-center gap-10">
-          <div className="text-lg sm:text-2xl font-bold my-1 sm:my-0 px-4 sm:px-2 hover:border-2 border-white rounded-full 
-        transition-colors
-      dark:hover:border-black">
-            <a href="#inicio">
-              Inicio
-            </a>
-          </div>
-          <div className="text-lg sm:text-2xl font-bold my-1 sm:my-0 px-4 sm:px-2 hover:border-2 border-white rounded-full
-        transition-colors
-      dark:hover:border-black">
-            <a href="#sobre">
-              Sobre
-            </a>
-          </div>
-          <div className="text-lg sm:text-2xl font-bold my-1 sm:my-0 px-4 sm:px-2 hover:border-2 border-white rounded-full
-        transition-colors
-      dark:hover:border-black">
-            <a href="#projetos">
-              Projetos
-            </a>
-          </div>
-          <div className="text-lg sm:text-2xl font-bold my-1 sm:my-0 px-4 sm:px-2 hover:border-2 border-white rounded-full
-        transition-colors
-      dark:hover:border-black">
-            <a href='#contato'>
-              Contato
-            </a>
-          </div>
-          <div className="relative w-16" onClick={toggleMode}>
-            <button
-              className="w-8 h-8 bg-white bg-[url(../src/assets/moon.svg)] bg-no-repeat bg-center rounded-[50%] 
-            absolute z-[1] top-1/2 transition-all transform -translate-y-1/2
-            dark:bg-[url(../src/assets/sun.svg)] dark:transition dark:transform dark:translate-x-full">
-            </button>
-            <span className="block w-16 h-6 bg-white bg-opacity-20 border-2 border-white backdrop-blur-sm rounded-full
-          dark:bg-black dark:bg-opacity-10 dark:border-black">
-            </span>
+      <nav className="flex h-auto sm:h-16 w-full justify-center items-center fixed">
+        <div className="w-auto mt-2 sm:flex">
+          <div className="flex justify-center items-center gap-2 sm:gap-10">
+            <div className="text-lg sm:text-2xl font-bold my-1 sm:my-0 sm:px-2 hover:sm:border-2 border-white 
+                            rounded-full 
+                            transition-colors
+                          dark:hover:border-black">
+              <a href="#inicio">
+                Inicio
+              </a>
+            </div>
+            <div className="text-lg sm:text-2xl font-bold my-1 sm:my-0 sm:px-2 hover:sm:border-2 border-white 
+                            rounded-full
+                            transition-colors
+                          dark:hover:border-black">
+              <a href="#sobre">
+                Sobre
+              </a>
+            </div>
+            <div className="text-lg sm:text-2xl font-bold my-1 sm:my-0 sm:px-2 hover:sm:border-2 border-white 
+                            rounded-full
+                            transition-colors
+                          dark:hover:border-black">
+              <a href="#projetos">
+                Projetos
+              </a>
+            </div>
+            <div className="text-lg sm:text-2xl font-bold my-1 sm:my-0 sm:px-2 hover:sm:border-2 border-white 
+                            rounded-full
+                            transition-colors
+                          dark:hover:border-black">
+              <a href='#contato'>
+                Contato
+              </a>
+            </div>
+            <div className="relative w-16" onClick={toggleMode}>
+              <button
+                className="w-8 h-8 bg-white bg-[url(../src/assets/moon.svg)] bg-no-repeat bg-center rounded-[50%] 
+                           absolute z-[1] top-1/2 transition-all transform -translate-y-1/2
+                           dark:bg-[url(../src/assets/sun.svg)] dark:transition dark:transform dark:translate-x-full">
+              </button>
+              <span className="block w-16 h-6 bg-white bg-opacity-20 border-2 border-white backdrop-blur-sm 
+                              rounded-full
+                             dark:bg-black dark:bg-opacity-10 dark:border-black">
+              </span>
+            </div>
           </div>
         </div>
       </nav>
