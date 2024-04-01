3f55cf9dab432c1439993ce761d379a080a0021d
Brian Rangel <brian.rangel@hotmail.com>
2024-04-01 20:31:51 +0100

    update tags anchors

--- src/components/Pages/contact.tsx
+++ src/components/Pages/contact.tsx
@@ -47,67 +47,56 @@ export function Contact() {
       className=" relative w-full h-screen flex flex-col items-center justify-center gap-10"
     >
       <p className="text-6xl font-bold">Contato</p>
-      <motion.div
+
+      <motion.a
         ref={ref}
         initial={{ y: "-100%" }}
         animate={up}
-        className="border-2 border-color-700 rounded-md w-[17.5rem] h-auto py-1 flex justify-center items-center
-      hover:bg-color-800 hover:border-color-600 transition-colors"
+        className="flex justify-center items-center gap-2 border-2 border-color-700 rounded-md  w-[17.5rem] h-14 py-1
+          hover:bg-color-800 hover:border-color-600 transition-colors"
+        href="https://www.linkedin.com/in/brianrangel"
+        target="_blank"
       >
-        <a
-          className="flex gap-2 items-center justify-center py-1"
-          href="https://www.linkedin.com/in/brianrangel"
-          target="_blank"
-        >
-          <LinkedinLogo size={35} weight="fill" />
-          <p className="font-bold text-xl">Linkedin</p>
-        </a>
-      </motion.div>
-      <motion.div
+        <LinkedinLogo size={35} weight="fill" />
+        <p className="font-bold text-xl">Linkedin</p>
+      </motion.a>
+
+      <motion.a
         initial={{ x: "-100%" }}
         animate={left}
-        className="border-2 border-color-700 rounded-md  w-[17.5rem] h-14 py-1 flex justify-center items-center
-      hover:bg-color-800 hover:border-color-600 transition-colors"
+        className="flex justify-center items-center gap-2 border-2 border-color-700 rounded-md  w-[17.5rem] h-14 py-1
+          hover:bg-color-800 hover:border-color-600 transition-colors"
+        href="https://www.instagram.com/brianrangel.dev"
+        target="_blank"
       >
-        <a
-          className="flex justify-center items-center gap-2"
-          href="https://www.instagram.com/brianrangel.dev"
-          target="_blank"
-        >
-          <InstagramLogo size={35} weight="fill" />
-          <p className="font-bold text-xl">Instagram</p>
-        </a>
-      </motion.div>
-      <motion.div
+        <InstagramLogo size={35} weight="fill" />
+        <p className="font-bold text-xl">Instagram</p>
+      </motion.a>
+
+      <motion.a
         initial={{ y: "100%" }}
         animate={down}
-        className="border-2 border-color-700 rounded-md  w-[17.5rem] h-14 py-1 flex justify-center items-center
-      hover:bg-color-800 hover:border-color-600 transition-colors"
+        className="flex justify-center items-center gap-2 border-2 border-color-700 rounded-md  w-[17.5rem] h-14 py-1
+          hover:bg-color-800 hover:border-color-600 transition-colors"
+        href="https://wa.me/351910025357"
+        target="_blank"
       >
-        <a
-          className="flex justify-center items-center gap-2"
-          href="https://wa.me/351910025357"
-          target="_blank"
-        >
-          <WhatsappLogo size={35} weight="fill" />
-          <p className="font-bold text-xl">Whatsapp</p>
-        </a>
-      </motion.div>
-      <motion.div
+        <WhatsappLogo size={35} weight="fill" />
+        <p className="font-bold text-xl">Whatsapp</p>
+      </motion.a>
+
+      <motion.a
         initial={{ x: "-100%" }}
         animate={left}
         className="border-2 border-color-700 rounded-md  w-[17.5rem] h-14 py-1 flex justify-center items-center
-      hover:bg-color-800 hover:border-color-600 transition-colors md:hidden"
+          hover:bg-color-800 hover:border-color-600 transition-colors md:hidden"
+        href="https://www.github.com/xumbreks"
+        target="_blank"
       >
-        <a
-          className="flex justify-center items-center gap-2"
-          href="https://www.github.com/xumbreks"
-          target="_blank"
-        >
-          <GithubLogo size={35} weight="fill" />
-          <p className="font-bold text-xl">Github</p>
-        </a>
-      </motion.div>
+        <GithubLogo size={35} weight="fill" />
+        <p className="font-bold text-xl">Github</p>
+      </motion.a>
+
       <span className="text-color-700 font-medium text-sm flex absolute bottom-2 left-1/2  -translate-x-1/2">
         Feito com ❤️ por Brian
       </span>
