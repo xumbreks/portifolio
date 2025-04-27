594edfeb5f5ee40c35ced9c51f3fd5682191e50f
Brian Rangel <brian.rangel@hotmail.com>
2025-04-27 15:14:58 +0100

    add a new font and photo in navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,3 +1,4 @@
+import brian from "@/assets/brian.png";
 import { Menu, X } from "lucide-react";
 import { useState } from "react";
 import { Button } from "./ui/button";
@@ -19,7 +20,7 @@ export function Navbar() {
 				<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
 					<div className="flex justify-between h-16">
 						<div className="flex items-center">
-							<span className="text-xl font-bold"> {"< Brian />"}</span>
+							<img src={brian} alt="foto de brian sorrindo" className="w-10" />
 						</div>
 
 						<div className="flex items-center space-x-4">
